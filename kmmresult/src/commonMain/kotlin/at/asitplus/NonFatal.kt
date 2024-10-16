2ffd3bdf75dca6be76bfa0c4d6d60071a7e6177f
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-10-16 13:33:31 +0200

    catchingUnwrapped

--- kmmresult/src/commonMain/kotlin/at/asitplus/NonFatal.kt
+++ kmmresult/src/commonMain/kotlin/at/asitplus/NonFatal.kt
@@ -1,5 +1,8 @@
 package at.asitplus
 
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
 /**
  * Throws any fatal exceptions. This is a re-implementation taken from Arrow's
  * [`nonFatalOrThrow`](https://apidocs.arrow-kt.io/arrow-core/arrow.core/non-fatal-or-throw.html) –
@@ -19,3 +22,21 @@ expect inline fun Throwable.nonFatalOrThrow(): Throwable
  * This helper hence provides the best of both worlds.
  */
 inline fun <T> Result<T>.nonFatalOrThrow(): Result<T> = this.onFailure { it.nonFatalOrThrow() }
+
+
+/**
+ * Non-fatal-only-catching version of stdlib's [runCatching], returning a [Result] --
+ * Re-throws any fatal exceptions, such as `OutOfMemoryError`. Relies on [Arrow](https://arrow-kt.io)'s
+ * [nonFatalOrThrow](https://apidocs.arrow-kt.io/arrow-core/arrow.core/non-fatal-or-throw.html) logic.
+ */
+@Suppress("TooGenericExceptionCaught")
+inline fun <T> catchingUnwrapped(block: () -> T): Result<T> = runCatching (block).nonFatalOrThrow()
+
+/**
+ * Non-fatal-only-catching version of stdlib's [runCatching] (calling the specified function [block] with `this` value
+ * as its receiver), directly returning a [Result] --
+ * Re-throws any fatal exceptions, such as `OutOfMemoryError`. Relies on [Arrow](https://arrow-kt.io)'s
+ * [nonFatalOrThrow](https://apidocs.arrow-kt.io/arrow-core/arrow.core/non-fatal-or-throw.html) logic.
+ */
+@Suppress("TooGenericExceptionCaught")
+inline fun <T, R> T.catchingUnwrapped(block: T.() -> R): Result<R> = runCatching (block).nonFatalOrThrow()
