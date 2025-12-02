6bcc961713c2c1bf20d8414804c3e8e76c68e89f
Jakob Heher <jakob.heher@iaik.tugraz.at>
2025-12-02 14:27:20 +0100

    make ready to merge

--- kmmresult/src/jvmTest/java/KmmResultTestJvm.kt
+++ kmmresult/src/jvmTest/java/KmmResultTestJvm.kt
@@ -6,8 +6,8 @@ import kotlin.test.Test
 class KmmResultTestJvm {
     @Test
     fun catchPathologicalUnitBehavior() {
-        codeSnippet("val x: at.asitplus.KmmResult<Unit> = at.asitplus.catching { 42 }.mapCatching { 42 }")
-                .shouldNotCompile("Initializer type mismatch")
-        }
+        // currently disabled, cf. https://github.com/kotest/kotest/issues/5244
+        /*codeSnippet("val x: at.asitplus.KmmResult<Unit> = at.asitplus.catching { 42 }.mapCatching { 42 }")
+                .shouldNotCompile("Initializer type mismatch")*/
     }
 }
